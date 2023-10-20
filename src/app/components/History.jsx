64bb1da23ae4dff53c511fd07f619983cab70bc1
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-10-20 19:10:37 -0300

    Se componetiza el historial

---
+++ src/app/components/History.jsx
@@ -0,0 +1,25 @@
+import { resumes } from "@/helpers/resumesExpample";
+import ResumeItem from "./ResumeItem";
+export default function History() {
+  return (
+    <>
+      <div className='text-text-custom-color-white mt-20 mb-4 md:mt-32'>
+        <h2 className='text-3xl'>
+          Hi <span className='text-text-custom-color-Details'>Lawyer!</span>
+        </h2>
+        <h4 className='text-2xl md:py-2'>Check your History!</h4>
+      </div>
+      <div className='max-h-screen overflow-y-auto w-full'>
+        <table className='min-w-full'>
+          <tbody>
+            {
+              resumes.map(resume => {
+                return <ResumeItem key={resume.id} resume={resume}/>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
