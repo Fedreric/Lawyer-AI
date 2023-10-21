1687c454ac56396883083f9f6aee1c82162ebc6e
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-10-21 16:42:12 -0300

    Se agrega el icono arrowDown

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -17,3 +17,9 @@ export const download = (
     <path d='M12 4l0 12'></path>
   </svg>
 );
+export const arrowDown = (
+  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-down" width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M6 9l6 6l6 -6"></path>
+</svg>
+);
