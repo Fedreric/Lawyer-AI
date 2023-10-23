1be8ea1a695347a57fc29f4a3188e25f2f282fb3
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-10-23 18:27:56 -0300

    Se corrigen estilos como el width y tamaño de la letra

--- src/app/ResultResume/page.jsx
+++ src/app/ResultResume/page.jsx
@@ -1,29 +1,39 @@
-'use client'
-import Link from "next/link"
+"use client";
+import { back } from "@/helpers/icons";
+import Link from "next/link";
 
 const DocGenerated = () => {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center p-4 sm:p-24 bg-bg-custom-color shadow-md">
-        <div className="p-4 mt-4">
-          <h1 className="text-black text-5xl text-center">Awesome! This is your contract resume</h1>
+      <main className='flex h-screen min-h-full flex-col items-center p-4 sm:p-24 bg-bg-custom-color shadow-md'>
+        <div className='p-4 mt-4'>
+          <h1 className='text-black text-3xl text-center'>
+            Awesome! This is your contract resume
+          </h1>
         </div>
-        <div className="w-full h-full custom-color-dark p-4 mt-4">
-          <textarea  rows={10} className=" w-full h-full text-white bg-custom-color-dark p-2" placeholder="Your contract resume text here"></textarea>
-          <div className="w-100 bg-custom-color-dark p-4 mt-4">
-          <button className="bg-custom-color-dark text-center text-white px-4 py-2 rounded hover:bg-blue-700">DOWNLOAD PDF</button>
+        <div className='w-full h-full custom-color-dark p-4 mt-4'>
+          <textarea
+            rows={8}
+            className=' w-full h-[80%] text-white bg-custom-color-dark p-2'
+            placeholder='Your contract resume text here'
+          ></textarea>
+          <section className='flex flex-col items-center md:flex-row md:justify-between gap-2'>
+            <div className='flex justify-center bg-custom-color-dark w-full rounded-sm'>
+              <button className='w-full bg-custom-color-dark text-center text-white px-4 py-2 rounded hover:bg-slate-900'>
+                DOWNLOAD PDF
+              </button>
+            </div>
+            <div className='bg-custom-color-dark flex justify-center items-center'>
+              <Link
+                href='/'
+                className='py-2 px-4 bg-custom-color-dark text-text-custom-color-white hover:bg-slate-900'
+              >
+                {back}
+              </Link>
+            </div>
+          </section>
         </div>
-        </div>
-        
-        <div className="p-4 mt-4  bg-custom-color-dark">
-        <button>
-          <Link href="/">
-             <spam className="bg-custom-color-dark hover:underline" >Back</spam>
-          </Link>
-        </button>
-      </div>
       </main>
-      
     </>
   );
 };
