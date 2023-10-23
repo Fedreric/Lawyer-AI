e66463387e79c4c01fa42e38afd23965ed962c68
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-10-23 18:28:39 -0300

    Se corrigen estilos y se agrega condicional si el usuario esta logueado o no

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,33 +1,46 @@
+"use client";
+import { login, user } from "@/helpers/icons";
 import Link from "next/link";
+import Logout from "./Logout";
 
 const Navbar = () => {
+  const usuario = sessionStorage.getItem("usuario");
   return (
     <div className='navbar bg-custom-color-dark text-text-custom-color-white fixed'>
       <div className='flex-1'>
-      <Link className='btn btn-ghost normal-case text-xl text-yellow-500 ' href={"/"}>
-        Lawyer-AI
+        <Link
+          className='btn btn-ghost normal-case text-xl text-text-custom-color-Details '
+          href={"/"}
+        >
+          Lawyer-AI
         </Link>
       </div>
       <div className='flex-none'>
         <ul className='menu menu-horizontal'>
           <li>
-            <Link href={"/"} className="">
-             Create new resume
-            </Link>  
+            <Link href={"/"} className='hover:text-text-custom-color-Details'>
+              Create new resume
+            </Link>
           </li>
           <li>
-            <details>
-              <summary>📜</summary>
-              <ul className='p-1 bg-base-100'>
-                <li>
-                  <Link href="/login">Log-in</Link>
-                </li>
-                <li>
-                  <Link href={"/register"}>Register</Link>
-                </li>
-                <li>
-                  <Link href={"/"}>New resume</Link>
-                </li>
+            <details className='md:mr-10'>
+              <summary className='hover:text-text-custom-color-Details'>
+                {user}
+              </summary>
+              <ul className='bg-custom-color-dark mt-10'>
+                {usuario ? (
+                  
+                  <Logout></Logout>
+                ) : (
+                  <li className='w-auto'>
+                    <Link
+                      href='/login'
+                      className='hover:text-text-custom-color-Details'
+                    >
+                      Login{login}
+                    </Link>
+                  </li>
+                )}
               </ul>
             </details>
           </li>
