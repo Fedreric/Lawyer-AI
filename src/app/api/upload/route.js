7494e5099e2fa5443c0cc00696e65cda830db040
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-10-30 19:16:04 -0300

    Se crea el endpoint al subir el pdf que transforma el pdf en un archivo local, lo guarda como pdf original y el resumen una vez listo

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,31 +1,61 @@
 import { writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 import path from "path";
 import PdfParse from "pdf-parse";
 
+const prisma = new PrismaClient();
+
 export async function POST(request) {
-  const data = await request.formData();
-  const file = data.get("file");
+    // extract data: PDF, userId
+   const data = await request.formData();
+   const file = data.get("file");
+   const userId = data.get("userId");
 
   if (!file) {
     return NextResponse.json({ success: false });
   }
 
+  //Convert data in buffer
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
-
+  //save pdf in local
   const filePath = path.join(process.cwd(), "public", file.name);
   writeFile(filePath, buffer);
 
-  console.log(`open ${filePath} to see the uploaded file`);
-
+  //extract text
   const pdf =  await PdfParse(buffer)
-  console.log(pdf.text)
+  //save original pdf in database
+  await prisma.originalDoc.create({
+    data:{
+        originalContent: pdf.text,
+        authorId: Number(userId)
+    }
+  })
+
+  //extract ID original pdf
+  const { originalId } = await prisma.originalDoc.findFirst({
+    where:{
+        originalContent: pdf.text
+    }
+  })
+  //logica resume
+
+  //------->
+
+  //save resume in database
+  await prisma.resume.create({
+    data:{
+        authorId: userId,
+        originalID: originalId,
+        name: 'Resume ' + file.name,
+        resumeContent: 'Resume content'
+    }
+  })
 
   return new Response(
     JSON.stringify({
-      message: "uploaded file",
-      path: filePath
+      message: "PDF Resume!"
     })
   );
 }
