319beda28a9b2a2aedc8f00565c22f294c5620ac
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-11-07 20:02:44 -0300

    Se crea el login con next-auth con credenciales

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/libs/db";
+import bcrypt from "bcrypt"
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "user@email.com" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, request) {
+        const userFound = await prisma.user.findUnique({
+            where:{
+                email: credentials.email
+            }
+        })         
+
+        if(!userFound) throw new Error('No user found')
+
+        const validPassword = bcrypt.compareSync(credentials.password, userFound.password)
+
+        if(!validPassword)throw new Error('No user found')
+
+        return {
+            id: userFound.id,
+            name: userFound.name,
+            email:userFound.email
+        }
+      }
+    })
+  ]
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
