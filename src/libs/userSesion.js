a5818dbca2079c3d58ed33dea3a7c27a92ff944a
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-11-07 21:03:36 -0300

    Se inteta refactorizar el getServerSession de authjs

---
+++ src/libs/userSesion.js
@@ -0,0 +1,7 @@
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// export const UserSesion = async ()=>{
+//     //   const sesion = await getServerSession(authOptions)
+//     return  await getServerSession(authOptions)
+// }
+
