2079314bb3de4f99883b7b7da0f62f06db1324c8
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-11-12 17:13:01 -0300

    Se agrera jwt a la hora de realizar el login

--- src/app/api/user/auth/route.js
+++ src/app/api/user/auth/route.js
@@ -2,49 +2,64 @@ import { NextResponse } from "next/server";
 import { helpers } from "../../services/helpers";
 import prisma from "@/libs/db";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+// import { serialize } from "cookie";
+import generateToken from "./token-sign";
 
+export async function POST(req) {
+  try {
+    const { email, password } = await req.json();
 
-export async function POST(request) {
-    try {
-      const { email, password } = await request.json();
-  
-      const existingUser = await prisma.user.findFirst({
-        where:{
-          email
-        }
-      })
-  
-      if(!existingUser){
-        return NextResponse.json({
-          message: 'Invalid email or password'
-        },{ status: 401 })
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email
       }
-  
-      const validPassword = bcrypt.compareSync(password, existingUser.password)
+    });
 
-      if(!validPassword){
-        return NextResponse.json({
-          message: 'Invalid email or password'
-        },{ status: 401 })
-      }
-  
-      return NextResponse.json({
-          message:'Log in',
-          userId: existingUser.userId,
-          name: existingUser.name
-      },{
-          status: 200
-      });
-    } catch (error) {
-      if(error.code === 'P2002'){
-          return NextResponse.json(
-              {
-                message: 'Invalid email or existent'
-              },
-              { status: 400 }
-            );
-      }
-      return helpers.catchError(error);
+    if (!existingUser) {
+      return NextResponse.json(
+        {
+          message: "Invalid email or password"
+        },
+        { status: 401 }
+      );
     }
+
+    const validPassword = bcrypt.compareSync(password, existingUser.password);
+    
+    if (!validPassword) {
+      return NextResponse.json(
+        {
+          message: "Invalid email or password"
+        },
+        { status: 401 }
+        );
+      }
+      
+    const { userId, name } = existingUser;
+    const payload = { userId, name, email };
+    const token = generateToken({payload});
+
+    return NextResponse.json(
+      {
+        userId,
+        name,
+        email,
+        token
+      },
+      {
+        status: 200
+      }
+    );
+  } catch (error) {
+    if (error.code === "P2002") {
+      return NextResponse.json(
+        {
+          message: "Invalid email or existent"
+        },
+        { status: 400 }
+      );
+    }
+    return helpers.catchError(error);
   }
-  
+}
