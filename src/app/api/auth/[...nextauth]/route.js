eb51646a245cc661e4e0974dc380037b5bc5f8df
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-11-13 16:21:40 -0300

    Update auth/route.js
    Se eliminan dependencias que no se usaban

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/libs/db";
-import bcrypt from "bcrypt";
 
 export const authOptions = {
   providers: [
