a6906654a0b7c43689df5ef19c56781f9e5c47f5
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-12-02 13:25:47 -0300

    se agrgan los iconos para el registro y para crear resumenes

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -38,6 +38,25 @@ export const deleteIcon = (
     <path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' />
   </svg>
 );
+export const createIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-square-rounded-plus'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z' />
+    <path d='M15 12h-6' />
+    <path d='M12 9v6' />
+  </svg>
+);
 export const arrowDown = (
   <svg
     xmlns='http://www.w3.org/2000/svg'
@@ -55,7 +74,7 @@ export const arrowDown = (
     <path d='M6 9l6 6l6 -6'></path>
   </svg>
 );
-export const logout = (
+export const logoutIcon = (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     className='icon icon-tabler icon-tabler-logout-2'
@@ -91,6 +110,26 @@ export const login = (
     <path d='M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2'></path>
     <path d='M3 12h13l-3 -3'></path>
     <path d='M13 15l3 -3'></path>
+  </svg>
+);
+export const registerIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-user-plus'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0' />
+    <path d='M16 19h6' />
+    <path d='M19 16v6' />
+    <path d='M6 21v-2a4 4 0 0 1 4 -4h4' />
   </svg>
 );
 export const user = (
