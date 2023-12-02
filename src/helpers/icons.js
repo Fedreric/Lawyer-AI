98a63f87900c64ffbf8bda9d0047af695cd6b5dd
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-12-01 23:48:13 -0300

    Se agrega el icono para eliminas

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,4 +1,4 @@
-export const download = (
+export const downloadIcon = (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     className='icon icon-tabler icon-tabler-download'
@@ -15,6 +15,27 @@ export const download = (
     <path d='M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2'></path>
     <path d='M7 11l5 5l5 -5'></path>
     <path d='M12 4l0 12'></path>
+  </svg>
+);
+export const deleteIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-trash'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M4 7l16 0' />
+    <path d='M10 11l0 6' />
+    <path d='M14 11l0 6' />
+    <path d='M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12' />
+    <path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' />
   </svg>
 );
 export const arrowDown = (
